713a3f1faadf10249fa77832ad78274eada37d9b
FerasHamam <feras.i.hamam@gmail.com>
2026-09-14 17:16:33 +0300

    fixed padding in weather dashboard

--- components/weather-dashboard.tsx
+++ components/weather-dashboard.tsx
@@ -163,7 +163,7 @@ export function WeatherDashboard({
     <main className="mx-auto flex min-h-screen w-full max-w-6xl flex-col px-5 py-8 sm:px-8 lg:px-12 lg:py-12">
       {header}
 
-      <section className="grid flex-1 gap-8 pt-10 pb-0 lg:grid-cols-[1.1fr_1.9fr] lg:items-start lg:py-0">
+      <section className="grid flex-1 gap-8 pt-10 pb-0 lg:grid-cols-[1.1fr_1.9fr] lg:items-start lg:pt-10">
         <WeatherSearch
           city={city}
           error={error}
